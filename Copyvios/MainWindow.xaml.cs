012854408bca641a8cb125d9e43f32d643267eaf
DavidWBrooks <38924413+DavidWBrooks@users.noreply.github.com>
2020-03-05 17:33:17 -0500

    Add code and screenshot

---
+++ Copyvios/MainWindow.xaml.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Documents;
+using System.Xml;
+
+namespace Copyvios
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly HttpClient client = new HttpClient();
+
+        double CruftHeight;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Tried doing this with async work but got weird cross-thread errors
+        private void StartComparison(object sender, RoutedEventArgs e)
+        {
+            DoComparison();
+        }
+
+        private void DoComparison()
+        {
+            DateTime starttime = DateTime.Now;
+            string article = articleTitle.Text;
+            string url = URL.Text;
+            if (String.IsNullOrWhiteSpace(article) || String.IsNullOrWhiteSpace(url)) {
+                MessageBox.Show("Provide both fields");
+                return;
+            }
+
+            if (!Regex.IsMatch(url, "^https?://", RegexOptions.IgnoreCase)) {
+                url = "https://en.wikisource.org/wiki/1911_Encyclopædia_Britannica/" + Uri.EscapeDataString(url.Replace(' ', '_'));
+            }
+
+            string wpAction =
+                "https://en.wikipedia.org/w/api.php?action=query&format=xml&prop=revisions&rvslots=main&rvprop=content&titles=" + 
+                article;
+            string ebhttp, wphttp;
+
+            string what = "Reading the URL: ";
+            try {
+                ebhttp = client.GetStringAsync(url).Result;
+                what = "Reading the Wikipedia article: ";
+                wphttp = client.GetStringAsync(wpAction).Result;
+            }
+            catch (AggregateException aex) {
+                MessageBox.Show(what + aex.InnerException.Message);
+                return;
+            }
+            catch (Exception ex) {
+                MessageBox.Show(what + ex.Message);
+                return;
+            }
+
+            string wpcontent, ebcontent;
+
+            try {
+                wpcontent = StripWP(wphttp);
+                ebcontent = StripEB(ebhttp);
+            }
+            catch (Exception ex) {  // surface any fatal error
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            Reload(wpcontent, ebcontent);
+
+            if (wpcontent.Length == 0 || ebcontent.Length == 0) {   // Weird, right?
+                Status("Nothing to see here");
+                return;
+            }
+
+            List<Chunk> wpchunks = Matcher.Reducer(wpcontent);
+            List<Chunk> ebchunks = Matcher.Reducer(ebcontent);
+
+            // Mark the chunks that match the opposite number
+            Matcher.Marker(wpchunks, ebchunks);
+
+            // Use the matched chunks to map to a bitmap of the original text
+            bool[] wpmap = new bool[wpcontent.Length];
+            bool[] ebmap = new bool[ebcontent.Length];
+
+            Matcher.Mapper(wpchunks, wpmap);
+            Matcher.Mapper(ebchunks, ebmap);
+
+            // Finally generate a sequence of Runs
+            IEnumerable<Run> wpruns = Matcher.Markup(wpcontent, wpmap);
+            IEnumerable<Run> ebruns = Matcher.Markup(ebcontent, ebmap);
+            Reload(wpruns, ebruns);
+
+            double elapsed = (DateTime.Now - starttime).TotalSeconds;
+            Status($"Total time: {elapsed:N1} seconds");
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            CruftHeight = this.ActualHeight - WPViewer.ActualHeight;
+            SizeChanged += Resized;
+
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1) articleTitle.Text = args[1];
+            if (args.Length > 2) {
+                URL.Text = args[2];
+                DoComparison();// WithCancel();
+            }
+        }
+
+        private string StripWP(string wphttp)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(wphttp);
+            XmlNodeList slotels = doc.GetElementsByTagName("slot");
+            if (slotels.Count == 0) {
+                throw new ApplicationException("Wikipedia query didn't return an article");
+            }
+            string wptext = slotels[0].InnerText;
+
+            wptext = Regex.Replace(wptext, "<!--.*?-->", "");
+            wptext = Regex.Replace(wptext, "<gallery.*?</gallery>", "", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            wptext = Regex.Replace(wptext, "\\[\\[Category.*?]]\n?", "", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            wptext = Regex.Replace(wptext, @"\[\[[^]]+\|(.+?)]]", (m)=> { return m.Groups[1].Value; }, RegexOptions.Singleline);
+            wptext = wptext.Replace("[", "").Replace("]", "");
+            wptext = Regex.Replace(wptext, @"<ref[^>]*/>", "", RegexOptions.IgnoreCase);
+            wptext = Regex.Replace(wptext, "<ref.+?</ref>", "", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            wptext = Regex.Replace(wptext, "'{2,}", "");
+
+            // Fun: walk a pair of characters at a time, detecting nesting {{}} sets
+            int templateDepth = 0;
+            StringBuilder wpsb = new StringBuilder(wptext.Length);
+            // Guard
+            wptext += '\n';
+
+            for (int i = 0; i < wptext.Length - 1; i++) {
+                char c1 = wptext[i];
+                char c2 = wptext[i + 1];
+                if (c1 == '{' && c2 == '{') templateDepth++;
+                if (templateDepth == 0) wpsb.Append(c1);
+                if (c1 == '}' && c2 == '}' && templateDepth > 0) {
+                    templateDepth--;
+                    i++;
+                }
+            }
+
+            wptext = Regex.Replace(wpsb.ToString(), @"^\s*\*\s*$", "", RegexOptions.Multiline);
+
+            // Finally do the trim thing
+            while (true) {
+                int len = wptext.Length;
+                wptext = wptext.Replace("\n\n\n", "\n\n");
+                if (wptext.Length == len) break;
+            }
+
+            return wptext.TrimStart('\n');
+        }
+
+        private string StripEB(string ebhttp)
+        {
+            string result;
+
+            // Extract body if there is one
+            Match m = Regex.Match(ebhttp, "<body[^>]*>(.*)</body>", RegexOptions.Singleline);
+            result = m.Success ? m.Groups[1].Value : ebhttp;
+
+            // Strip to the first para marker
+            int pmarker = result.IndexOf("<p>");
+            if (pmarker >= 0) {
+                result = result.Substring(pmarker + 3);
+            }
+
+            // Strip from the last end-paragraph marker
+            pmarker = result.LastIndexOf("</p>");
+            if (pmarker >= 0) {
+                result = result.Substring(0, pmarker);
+            }
+
+            // Replace paragraph markers with newline and remove most remaining HTML markup
+            // Does this do the right thing with italics etc?
+
+            result = result.Replace("<p>", "\n");
+            result = Regex.Replace(result, "<[^>]*>", "");
+            while (true) {
+                int len = result.Length;
+                result = result.Replace("\n\n\n", "\n\n");
+                if (result.Length == len) break;
+            }
+
+            return result;
+        }
+
+        private void Status(string message)
+        {
+            Progress.Content = message;
+        }
+
+        private void Reload(IEnumerable<Run> wpruns, IEnumerable<Run> ebruns)
+        {
+            WPPara.Inlines.Clear();
+            WPPara.Inlines.AddRange(wpruns);
+            EBPara.Inlines.Clear();
+            EBPara.Inlines.AddRange(ebruns);
+        }
+
+        private void Reload(string wpstring, string ebstring)
+        {
+            Reload(new[] { new Run(wpstring) }, new[] { new Run(ebstring) });
+        }
+
+        private void Resized(object sender, SizeChangedEventArgs e)
+        {
+            double newHeight = this.ActualHeight - CruftHeight;
+            WPViewer.Height = newHeight;
+            EBViewer.Height = newHeight;
+        }
+    }
+}
---
+++ Copyvios/Matcher.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace Copyvios
+{
+    // A single word
+    public class Word
+    {
+        public int startpos;
+        public int len;
+        public int hash;
+        public Word(int s, int l, string word)
+        {
+            startpos = s;
+            len = l;
+            hash = word.GetHashCode();
+        }
+    }
+
+    // a single nGram
+    public class Chunk
+    {
+        public int startpos;   // Position in the original text
+        public int len;        // Length in the original text
+        public Int64 hash;
+        public bool isMatch;
+
+        public Chunk(int s, int l, Int64 h)
+        {
+            startpos = s;
+            len = l;
+            hash = h;
+            isMatch = false;
+        }
+    }
+
+    public static class Matcher
+    {
+        const int minGram = 3;
+        const int maxGram = 5;
+
+        static readonly string[] smallwords = new[] { "a", "an", "the", "in", "it", "at", "to", "he", "she", "as", "is", "of" };
+        static readonly int[] smallhashes = smallwords.Select(w => w.GetHashCode()).ToArray();
+        const Int64 guardhash = 0;
+        static readonly Brush highlighter = new SolidColorBrush(Color.FromRgb(0xFF, 0xAA, 0xAA));
+
+        // Do a quadratic match (it's OK, I'll wait) on the chunks
+        public static void Marker(List<Chunk> wpchunks, List<Chunk> ebchunks)
+        {
+            foreach (Chunk wpchunk in wpchunks) {
+                foreach (Chunk ebchunk in ebchunks) {
+                    if (wpchunk.hash == ebchunk.hash) {
+                        wpchunk.isMatch = ebchunk.isMatch = true;
+                    }
+                }
+            }
+        }
+
+        // Consolidate chunks into an array of on/off settings
+        public static void Mapper(List<Chunk> chunks, bool[] bitmap)
+        {
+            foreach(Chunk chunk in chunks.Where(c => c.isMatch)) {
+                int ind = chunk.startpos;
+                int length = chunk.len;
+                while (length-- > 0) {
+                    bitmap[ind++] = true;
+                }
+            }
+        }
+
+        // Produce a list of nGrams
+        public static List<Chunk> Reducer(string text)
+        {
+            Word[] wordlist = WordReduce(text);
+            List<Chunk> result = new List<Chunk>(wordlist.Length + 1);
+
+            // Work through word sequences, ending 3 away from the end of the list
+            // Yes, adjacent grams will have overlapping words.
+
+            for (int i = 0; i <= (wordlist.Length - maxGram); i++) {
+                if (smallhashes.Contains(wordlist[i].hash)) continue;   // Dom't start on a common word
+                int start = wordlist[i].startpos;
+                Int64 newgram = 0;
+                int gramlength = 0;
+                int wordsingram = 0;
+                for (int j = 0; j < maxGram; j++) {
+                    Word thisword = wordlist[i + j];
+                    int h = thisword.hash;
+                    if (!smallhashes.Contains(h)) {
+                        // If you don't shift-left, the words can be in any order. This is why I'm using Int64.
+                        // But forget the shift if you actually want the "unordered" logic.
+                        newgram = (newgram << 8) ^ h;
+                        gramlength = thisword.startpos - start + thisword.len;
+                        if (++wordsingram == minGram)
+                            break;
+                    }
+                }
+
+                if (newgram != 0) {
+                    result.Add(new Chunk(start, gramlength, newgram));
+                }
+            }
+
+            result.Add(new Chunk(text.Length, 0, guardhash));   // Guard
+            return result;
+        }
+
+        // Produce a list of words as hashes
+        static Word[] WordReduce(string text)
+        {
+            MatchCollection matches = Regex.Matches(text.ToLower(), @"[-\w]+");
+            List<Word> result = new List<Word>(matches.Count);
+            foreach (Match m in matches) {
+                result.Add(new Word(m.Index, m.Length, m.Value.Replace("-", "")));
+            }
+
+            // Some guards that should work with the above logic
+            for (int i = minGram; i < maxGram; i++) {
+                result.Add(new Word(text.Length, 0, "a"));
+            }
+            return result.ToArray();
+        }
+
+        // Provide a sequence of Runs
+        internal static IEnumerable<Run> Markup(string content, bool[] map)
+        {
+            // I think we already eliminated this, but to be sure...
+            if (content.Length == 0) {
+                yield break;
+            }
+
+            // What color to start?
+            bool runMarked = map[0];
+            int runpos = 0;
+            int pos = 0;
+            while (++pos < content.Length) {
+                bool thisMarked = map[pos];
+                if (thisMarked != runMarked) {
+                    Run newrun = new Run(content.Substring(runpos, pos - runpos));
+                    if (runMarked) newrun.Background = highlighter;
+                    yield return newrun;
+                    runpos = pos;
+                    runMarked = thisMarked;
+                }
+            }
+
+            Run finalrun = new Run(content.Substring(runpos, pos - runpos));
+            if (runMarked) finalrun.Background = highlighter;
+            yield return finalrun;
+        }
+    }
+}
